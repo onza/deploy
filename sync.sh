e7bb55e49ab80193e49481fccb46ce339ac63f72
onza <info@martinfarkas.com>
2022-07-08 13:15:05 +0200

    refactor

--- sync.sh
+++ sync.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-# deploy script: customize username and remote_path and copy script to the target directory. running over terminal with "sh deploy.sh"
+# deploy script: customize variables and run 'sh sync.sh'
 username="[username]@[host]"
-remote_path="/absolute/path/to/destination/project_directory/"
+remote_path="/absolute/path/to/destination/project_directory_name/"
 
-# customize name of the target directory, e.g. the typo3 provider extension
 local_path="./"
-excludestr="--exclude 'package.json' --exclude 'package-lock.json' --exclude '.git' --exclude '.gitignore' --exclude '.vscode' --exclude '.DS_Store' --exclude 'mf_provider/node_modules' --exclude 'gulpfile.js' --exclude '*/Resources/Template/Scss' --exclude 'deploy.sh'"
+
+excludestr="
+  --exclude '.vscode'
+  --exclude '.DS_Store'
+  --exclude 'sync.sh'
+  --exclude '.git'
+  --exclude '.gitignore'
+  --exclude 'gulpfile.js'
+  --exclude 'package.json'
+  --exclude 'package-lock.json'
+  --exclude 'node_modules'
+"
 
 cmd="rsync -avz --delete --force $excludestr -e ssh $local_path ${username}:${remote_path}"
 echo $cmd
