611abbb89f184ef82015cb14328627f7ae6dc0cc
onza <info@martinfarkas.com>
2022-07-08 10:04:30 +0200

    renamed, cleaned paths

--- sync.sh
+++ sync.sh
@@ -2,10 +2,10 @@
 
 # deploy script: customize username and remote_path and copy script to the target directory. running over terminal with "sh deploy.sh"
 username="[username]@[host]"
-remote_path="/absolute/path/to/destination/project_directory/typo3conf/ext/"
+remote_path="/absolute/path/to/destination/project_directory/"
 
 # customize name of the target directory, e.g. the typo3 provider extension
-local_path="../[provider_extension]"
+local_path="./"
 excludestr="--exclude 'package.json' --exclude 'package-lock.json' --exclude '.git' --exclude '.gitignore' --exclude '.vscode' --exclude '.DS_Store' --exclude 'mf_provider/node_modules' --exclude 'gulpfile.js' --exclude '*/Resources/Template/Scss' --exclude 'deploy.sh'"
 
 cmd="rsync -avz --delete --force $excludestr -e ssh $local_path ${username}:${remote_path}"
